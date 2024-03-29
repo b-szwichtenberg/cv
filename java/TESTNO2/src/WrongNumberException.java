6dd7b3768f0633ef461a8e7ebe530ae1c9c92408
b-szwichtenberg <107558304+b-szwichtenberg@users.noreply.github.com>
2024-03-30 00:59:58 +0100

    Delete java directory

--- java/TESTNO2/src/WrongNumberException.java
+++
@@ -1,9 +0,0 @@
-public class WrongNumberException extends Exception {
-    String s;
-    WrongNumberException(String s){
-        this.s = s;
-    }
-    public String getMessage(){
-        return s;
-    }
-}
--- java/exeptions2/src/TooYoung.java
+++
@@ -1,9 +0,0 @@
-public class TooYoung extends  Exception{
-    String s;
-    TooYoung(String s){
-        this.s=s;
-    }
-    public String getMessage(){
-        return s;
-    }
-}
